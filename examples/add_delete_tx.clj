38537421a3f1645f7b9506a109c0db9693f0c6ab
Ben Sless <ben.sless@gmail.com>
2023-09-04 12:13:00 +0300

    Move example to examples directory

---
+++ examples/add_delete_tx.clj
@@ -0,0 +1,75 @@
+(ns add-delete-tx
+  (:require
+   [clojure.test.check :as tc]
+   [clojure.test.check.generators :as gen]
+   [clojure.test.check.properties :as prop]
+   [fsm-test-check.core :as fsm]))
+
+(def add-cmd
+  (reify
+    fsm/Command
+    (precondition [_ state]
+      (vector? (:people state)))
+
+    (postcondition [_ state cmd]
+      ;;add only valid if no other person with same id
+      (->> (:people state)
+           (filter #(= (:id %) (:id cmd)))
+           seq
+           nil?))
+
+    (exec [_ state cmd]
+      (update-in state [:people] (fn [people]
+                                   (conj people
+                                         (dissoc cmd :type)))))
+
+    (generate [_ state]
+          (gen/fmap (partial zipmap [:type :name :id])
+                    (gen/tuple (gen/return :add-cmd)
+                               (gen/not-empty gen/string-alphanumeric)
+                               (gen/such-that #(-> (mapv :id (:people state))
+                                                   (contains? %)
+                                                   not)
+                                              gen/int))))))
+
+(def delete-cmd
+  (reify
+    fsm/Command
+    (precondition [_ state]
+      (seq (:people state)))
+
+    (postcondition [_ state cmd]
+      ;;delete only valid if existing person with id
+      (->> (:people state)
+           (filter #(= (:id %) (:id cmd)))
+           seq))
+
+    (exec [_ state cmd]
+      (update-in state [:people] (fn [people]
+                                   (vec (filter #(not= (:id %)
+                                                       (:id cmd))
+                                                people)))))
+
+    (generate [_ state]
+      (gen/fmap (partial zipmap [:type :id])
+                (gen/tuple (gen/return :delete-cmd)
+                           (gen/elements (mapv :id (:people state))))))))
+
+;;-----------------------------------------------------
+;;property definition
+
+(defn apply-tx
+  "Apply transactions fails when there are two delete commands"
+  [tx-log]
+  (->> tx-log
+       (filter #(= :delete-cmd (:type %)))
+       count
+       (> 2)))
+
+(def commands-consistent-apply
+  (prop/for-all [tx-log (fsm/cmd-seq {:people []} {:add-cmd add-cmd :delete-cmd delete-cmd})]
+                (true? (apply-tx tx-log))))
+
+(comment
+  (tc/quick-check 100 commands-consistent-apply)
+  )
--- src/fsm_test_check/core.clj
+++ src/fsm_test_check/core.clj
@@ -1,8 +1,7 @@
 (ns fsm-test-check.core
-  (:require [clojure.test.check :as tc]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]
-            [clojure.test.check.rose-tree :as rose]))
+  (:require
+   [clojure.test.check.generators :as gen]
+   [clojure.test.check.rose-tree :as rose]))
 
 (defprotocol Command
   (precondition [this state] "Returns true if command can be applied in current system state")
@@ -64,75 +63,3 @@
                                                           (mapv second cmd-seq)
                                                           commands)]
                             (gen/gen-pure shrinked)))))))
-
-;;-----------------------------------------------------
-;;commands
-
-(def add-cmd
-  (reify
-    Command
-    (precondition [_ state]
-      (vector? (:people state)))
-
-    (postcondition [_ state cmd]
-      ;;add only valid if no other person with same id
-      (->> (:people state)
-           (filter #(= (:id %) (:id cmd)))
-           seq
-           nil?))
-
-    (exec [_ state cmd]
-      (update-in state [:people] (fn [people]
-                                   (conj people
-                                         (dissoc cmd :type)))))
-
-    (generate [_ state]
-          (gen/fmap (partial zipmap [:type :name :id])
-                    (gen/tuple (gen/return :add-cmd)
-                               (gen/not-empty gen/string-alphanumeric)
-                               (gen/such-that #(-> (mapv :id (:people state))
-                                                   (contains? %)
-                                                   not)
-                                              gen/int))))))
-
-(def delete-cmd
-  (reify
-    Command
-    (precondition [_ state]
-      (seq (:people state)))
-
-    (postcondition [_ state cmd]
-      ;;delete only valid if existing person with id
-      (->> (:people state)
-           (filter #(= (:id %) (:id cmd)))
-           seq))
-
-    (exec [_ state cmd]
-      (update-in state [:people] (fn [people]
-                                   (vec (filter #(not= (:id %)
-                                                       (:id cmd))
-                                                people)))))
-
-    (generate [_ state]
-      (gen/fmap (partial zipmap [:type :id])
-                (gen/tuple (gen/return :delete-cmd)
-                           (gen/elements (mapv :id (:people state))))))))
-
-;;-----------------------------------------------------
-;;property definition
-
-(defn apply-tx
-  "Apply transactions fails when there are two delete commands"
-  [tx-log]
-  (->> tx-log
-       (filter #(= :delete-cmd (:type %)))
-       count
-       (> 2)))
-
-(def commands-consistent-apply
-  (prop/for-all [tx-log (cmd-seq {:people []} {:add-cmd add-cmd :delete-cmd delete-cmd})]
-                (true? (apply-tx tx-log))))
-
-(comment
-  (tc/quick-check 100 commands-consistent-apply)
-  )
